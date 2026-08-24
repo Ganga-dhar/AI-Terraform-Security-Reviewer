130a6a6dda6a7939deb025a8235c0c19c1ddbe85
advita.vedanta01@gmail.com <advita.vedanta01@gmail.com>
2026-08-24 17:52:13 +0530

    llm fix

--- auditor/llm_client.py
+++ auditor/llm_client.py
@@ -1,29 +1,17 @@
-# import os
-
-# import boto3
-
-
-# def create_client():
-
-#     region = os.getenv(
-#         "AWS_REGION",
-#         "us-east-1"
-#     )
-
-#     return boto3.client(
-#         "bedrock-runtime",
-#         region_name=region,
-#     )
+import os
 
 import boto3
-from langchain_aws import ChatBedrockConverse
 
 
 def create_client():
 
-    return ChatBedrockConverse(
-        model="us.amazon.nova-lite-v1:0",
-        region_name="us-east-1",
-        temperature=0,
+    region = os.getenv(
+        "AWS_REGION",
+        "us-east-1"
     )
-   
+
+    return boto3.client(
+        "bedrock-runtime",
+        region_name=region,
+    )
+    
