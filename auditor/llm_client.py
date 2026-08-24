22ac4eec27850cf6d09c0f4f19ff994387ba9543
advita.vedanta01@gmail.com <advita.vedanta01@gmail.com>
2026-08-24 17:44:40 +0530

    updated LLM model to langchain format

--- auditor/llm_client.py
+++ auditor/llm_client.py
@@ -1,16 +1,29 @@
-import os
+# import os
+
+# import boto3
+
+
+# def create_client():
+
+#     region = os.getenv(
+#         "AWS_REGION",
+#         "us-east-1"
+#     )
+
+#     return boto3.client(
+#         "bedrock-runtime",
+#         region_name=region,
+#     )
 
 import boto3
+from langchain_aws import ChatBedrockConverse
 
 
 def create_client():
 
-    region = os.getenv(
-        "AWS_REGION",
-        "us-east-1"
+    return ChatBedrockConverse(
+        model="us.amazon.nova-lite-v1:0",
+        region_name="us-east-1",
+        temperature=0,
     )
-
-    return boto3.client(
-        "bedrock-runtime",
-        region_name=region,
-    )
+   
